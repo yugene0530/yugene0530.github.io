73ce1c4698e196637c0046feb61ba49754da5299
Yugene <aresvampire@mail.com>
2021-10-29 03:15:57 -0400

    update site

---
+++ precache-manifest.f81927c8d7c3fc7ff63da3342ed64578.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb17838afba2657ad1bf",
+    "url": "/static/css/main.28d125a7.chunk.css"
+  },
+  {
+    "revision": "fb17838afba2657ad1bf",
+    "url": "/static/js/main.fb17838a.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "3d8e830eb92bd6c6f356",
+    "url": "/static/css/2.7976b7ce.chunk.css"
+  },
+  {
+    "revision": "3d8e830eb92bd6c6f356",
+    "url": "/static/js/2.3d8e830e.chunk.js"
+  },
+  {
+    "revision": "be227b3379e997fcc293a2b0db305c35",
+    "url": "/static/media/me.be227b33.png"
+  },
+  {
+    "revision": "1ad917eea8b4c921992d925cac64490f",
+    "url": "/static/media/raleway-bold-webfont.1ad917ee.woff2"
+  },
+  {
+    "revision": "50a248dc33ced2bbd235a7a9a473825b",
+    "url": "/static/media/raleway-regular-webfont.50a248dc.woff2"
+  },
+  {
+    "revision": "474c1ebf2a0a463d9a03a29a065ae8f2",
+    "url": "/static/media/raleway-bold-webfont.474c1ebf.woff"
+  },
+  {
+    "revision": "1d574968db21bd1eb692175bbe77dd32",
+    "url": "/static/media/raleway-semibold-webfont.1d574968.woff2"
+  },
+  {
+    "revision": "34173a45af2c9b30a252fbe77c5364c8",
+    "url": "/static/media/raleway-semibold-webfont.34173a45.woff"
+  },
+  {
+    "revision": "dda65b551e6241174edd384d2a44085b",
+    "url": "/static/media/raleway-regular-webfont.dda65b55.woff"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "e55a9e2f869e2bc631d33ecd056ddde3",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1f4e037b0bab80f9cb05f9e2e3c85847.js"
+  "/precache-manifest.f81927c8d7c3fc7ff63da3342ed64578.js"
 );
 
 workbox.clientsClaim();
